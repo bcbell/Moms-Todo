61603f5a0eb9ccba2882d0f20418374c8c163fa1
bcbell <coleman.britt@gmail.com>
2022-05-18 23:56:07 -0400

    -moms-to-dos

--- src/App.js
+++ src/App.js
@@ -38,7 +38,6 @@ function App() {
     function deleteList(id){
       const newArray = items.filter(item => item.id !== id);
       setItems(newArray);
-      // console.log(id);
     }
  
 
@@ -56,13 +55,13 @@ function App() {
 
         <button onClick={()=> addItem()}>Add</button>
   
-        <ul>
-          {items.map( item =>{
+        <ol>
+          {items.map(item => {
             return(
-              <li key={item.id}>{item.value} <button onClick={()=>deleteList()}>Delete</button></li>
+              <li key={item.id}>{item.value} <button onClick={()=> deleteList(item.id)}>Delete</button></li>
             )
           })}
-        </ul>
+        </ol>
 
     </div>
   );
