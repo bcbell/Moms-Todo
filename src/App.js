85a741fbad604c6511871e3624a1cf33ada29c43
bcbell <coleman.britt@gmail.com>
2022-05-20 23:47:29 -0400

    -moms-to-dos

--- src/App.js
+++ src/App.js
@@ -58,7 +58,7 @@ function App() {
         <ol>
           {items.map(item => {
             return(
-              <li key={item.id}>{item.value} <button onClick={()=> deleteList(item.id)}>Delete</button></li>
+              <li key={item.id}><input type="checkbox"/>{item.value} <button onClick={()=> deleteList(item.id)}>Delete</button></li>
             )
           })}
         </ol>
